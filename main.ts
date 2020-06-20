ea48b6f8f00dfeff433e87b40139f28ef310df5c
Alejandra Kozub Pérez <51513251+akozubperez@users.noreply.github.com>
2020-06-20 16:17:59 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,29 @@
+let numero_elegido = 0
+input.onGesture(Gesture.Shake, function () {
+    numero_elegido = randint(1, 3)
+    if (numero_elegido == 1) {
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            `)
+    } else if (numero_elegido == 2) {
+        basic.showLeds(`
+            # # . . #
+            # # . # .
+            . . # . .
+            # # . # .
+            # # . . #
+            `)
+    } else {
+        basic.showLeds(`
+            . # # # .
+            # # # # #
+            # # # # #
+            # # # # #
+            . # # # .
+            `)
+    }
+})
